9a0d7df3801076a49db38e298b942cdb2929b0e1
LUCAS VICENTE PRADO <lucas.prado59@etec.sp.gov.br>
2020-11-17 21:12:01 -0300

    Página "redação" atualizada com o bootstrap

--- resources/views/redacao.blade.php
+++ resources/views/redacao.blade.php
@@ -1,8 +1,11 @@
 <!doctype html>
 <html>
     <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <title>Vestibulando</title>
-        <meta charset="utf-8">   
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+        <link rel="stylesheet" href="/css/custom.css" /> 
         <link href="/css/redacao.css" rel="stylesheet">
     </head>
     <body>
@@ -10,18 +13,43 @@
             <div id=headerph>
             <header id=cabecalho>
                 <img  src="/img/logo.png">
-                <nav id=menu>
-                    <div class=links>
-                        <a href= "/welcome"><c>Home</c></a>
-                        <a href= "/welcome">Universidades</a>
-                        <a href= "/welcome">Redações</a>
-                        <a href= "/welcome">Quem Somos</a>
-                        <a href= "/welcome">Cadastro/Login</a>
-                        <input type="text" placeholder="Pesquise no Vestibulando">
-                        <input type="submit" value="">
+                <nav class="navbar navbar-expand-lg navbar-light">
+            <a class="navbar-brand d-lg-none" href="#">
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
 
-                    </div>
-                </nav>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <nav id=menu>
+                        <ul class="navbar-nav mr-auto">
+                         <li class="nav-item">
+                              <a class="nav-link" href="#">Home</a>
+                         </li>
+                         <li class="nav-item">
+                             <a class="nav-link" href="#">Universidades</a>
+                         </li>
+                         <li class="nav-item">
+                              <a class="nav-link" href="#">Redações</a>
+                            </li>
+                         <li class="nav-item">
+                             <a class="nav-link" href="#">Quem Somos</a>
+                          </li>
+                          <li class="nav-item">
+                                <a class="nav-link" href="#">Cadastro/Login</a>
+                         </li>
+                        </ul>
+                    </nav>
+                    <form class="form-inline my-2 my-lg-0">
+                        <div class="input-group">
+                         <input type="search" class="form-control" placeholder="Pesquisar" aria-label="Search" aria-describedby="button-addon2">
+                            <div class="input-group-append">
+                                <button class="btn btn-outline-secondary" type="button" id="button-addon2"><i class="fa fa-search"></i></button>
+                          </div>
+                     </div>
+                 </form>
+                </div>
+            </nav>
             </header>
             </div>
             
@@ -57,5 +85,9 @@
             </div>
             <b></b>
         </div>
+        <script src="https://use.fontawesome.com/edfa239d55.js"></script>
+            <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+            <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
     </body>
 </html>
