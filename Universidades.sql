a39978cfa8cbbc7841c4f598cf3aa82bfb9af763
Guilherme1313 Oliveira <guilherme.augusto.oliveira2003@gmail.com>
2020-11-27 21:17:16 -0300

    pdf coisas

--- Universidades.sql
+++ Universidades.sql
@@ -1,12 +1,19 @@
 use  laravel;
-select * from universidades;
 
-delete from universidades where id='6';
 INSERT INTO universidades VALUES (01, '2020-11-26','2020-11-26', 'USP', 'Universidade de São Paulo', 'https://www5.usp.br/', 'São Paulo', 'img\usp_logo.jpg', '\editais\Edital Vestibular USP 2021.pdf');
-INSERT INTO universidades VALUES (02, '2020-11-26', '2020-11-26', 'FIO', 'Faculdades Integradas de Ourinhos', 'https://www.unifio.edu.br/home/', 'Ourinhos', 'img\FIO_LOGO.jpg', '\editais\Edital Vestibular FIO 2020.pdf');
-INSERT INTO universidades VALUES (03, '2020-11-26', '2020-11-26', 'UNESP', 'Universidade Estadual Paulista "Júlio de Mesquita Filho"', 'https://www2.unesp.br/', 'São Paulo', 'img\unesplojo.png', '\editais\Edital Vestibular Unesp 2020.pdf'); 
-INSERT INTO universidades VALUES (04, '2020-11-26', '2020-11-26', 'UNICAMP', 'Universidade Estadual de Campinas', 'https://www.unicamp.br/unicamp/', 'Campinas', 'img\unicamp.png', '\editais\Edital Vestibular Unicamp 2021.pdf'); 
-INSERT INTO universidades VALUES (05, '2020-11-26', '2020-11-26', 'UEM', 'Universidade Estadual de Maringá', 'http://www.uem.br/', 'Maringá','img\uem_logo.jpg', '\editais\Edital Vestibular UEM 2020.pdf');
+INSERT INTO universidades VALUES (02, '2020-11-26', '2020-11-27', 'UNESP', 'Universidade Estadual Paulista "Júlio de Mesquita Filho"', 'https://www2.unesp.br/', 'São Paulo', 'img\unesplojo.png', '\editais\Edital Vestibular Unesp 2020.pdf'); 
+INSERT INTO universidades VALUES (03, '2020-11-26', '2020-11-27', 'UNICAMP', 'Universidade Estadual de Campinas', 'https://www.unicamp.br/unicamp/', 'Campinas', 'img\unicamp.png', '\editais\Edital Vestibular Unicamp 2021.pdf'); 
+INSERT INTO universidades VALUES (04, '2020-11-26', '2020-11-27', 'UEM', 'Universidade Estadual de Maringá', 'http://www.uem.br/', 'Maringá','img\uem_logo.jpg', '\editais\Edital Vestibular UEM 2020.pdf');
 
-select * from provas;
+INSERT INTO provas VALUES (01, '2020-11-27', '2020-11-27', '2018', 'provas\Prova USP 2018.pdf', 'gabaritos\Gabarito USP 2018.pdf', 01);
+INSERT INTO provas VALUES (02, '2020-11-27', '2020-11-27', '2019', 'provas\Prova USP 2019.pdf', 'gabaritos\Gabarito USP 2019.pdf', 01);
+INSERT INTO provas VALUES (03, '2020-11-27', '2020-11-27', '2019', 'provas\Prova UNESP Primeira Fase 2019.pdf', 'gabaritos\Gabarito UNESP Primeira Fase 2019.pdf', 02);
+INSERT INTO provas VALUES (04, '2020-11-27', '2020-11-27', '2020', 'provas\Prova UNESP 2020.pdf', 'gabaritos\Gabarito UNESP 2020.pdf', 02);
+INSERT INTO provas VALUES (05, '2020-11-27', '2020-11-27', '2019', 'provas\Prova UNICAMP Primeira Fase (Q e X) 2019.pdf', 'gabaritos\Gabarito UNICAMP Primeira Fase 2019.pdf', 03);
+INSERT INTO provas VALUES (06, '2020-11-27', '2020-11-27', '2019', 'provas\Prova UNICAMP Segunda Fase 2019.pdf', 'gabaritos\Gabarito UNICAMP Segunda Fase 2019.pdf', 03);
+INSERT INTO provas VALUES (07, '2020-11-27', '2020-11-27', '2019', 'provas\Prova UEM Inverno 2019.pdf', 'gabaritos\Gabarito UEM Inverno 2019.pdf', 04);
+INSERT INTO provas VALUES (08, '2020-11-27', '2020-11-27', '2019', 'provas\Prova UEM Verao 2019.pdf', 'gabaritos\Gabarito UEM Verao 2019.pdf', 04);
+
+
+
 
