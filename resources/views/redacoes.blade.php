aad036cb91e0f953839ab24ad965bffb57327562
GUSTAVO GRANDISOLLI ZWICKER <gustavo.zwicker@etec.sp.gov.br>
2020-11-30 14:48:56 -0300

    redacoes

--- resources/views/redacoes.blade.php
+++ resources/views/redacoes.blade.php
@@ -10,28 +10,32 @@
                 <div id=retangulo>
                     <h1>REDAÇÕES</h1>
                 </div>
-
+            <div class="row">
+                <div class="col-sm-6">
                 <h2>ENEM</h2>
                 <div id = quadrado1>
                     <a href="http://127.0.0.1:8000/redacao_enem">
                     <img src="/img/red_enem_pequena.png">
                     </a>
                 </div>
-
+            </div>
+            <div class="col-sm-6">
                 <h4>FUVEST</h4>
                 <div id = quadrado3>
                     <a href="http://127.0.0.1:8000/redacao_fuv">
                     <img src="/img/red_fuvest_pequena.png">
                     </a>
                 </div>
-                    
+            </div>
+            <div class="col-sm-6">
                 <h3>UNICAMP</h3>
                 <div id = quadrado2>
                     <a href="http://127.0.0.1:8000/redacao_unicamp">
                     <img src="/img/red_unicamp_pequena.png">
                     </a>
                 </div>
-
+            </div>
+            <div class="col-sm-6">
                 <h5>PUC-RJ</h5>
                 <div id = quadrado4>
                     <a href="http://127.0.0.1:8000/redacao_puc">
@@ -39,4 +43,6 @@
                     </a>
                 </div>
             </div>
+            </div>
+        </div>
 @endsection
